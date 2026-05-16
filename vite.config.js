16ddcaf7ee4864fdd30f27cf76767f3898fc1ad9
clovenbradshaw-ctrl <clovenbradshaw@gmail.com>
2026-05-16 13:34:38 -0500

    Stabilize docs asset names

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "./",
   plugins: [react()],
-  build: { outDir: "docs" },
+  build: {
+    outDir: "docs",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name][extname]",
+      },
+    },
+  },
 });
