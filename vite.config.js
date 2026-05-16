11d93869f14b82b74938161a0e9f5c440d93c50c
clovenbradshaw-ctrl <clovenbradshaw@gmail.com>
2026-05-16 13:37:32 -0500

    Remove generated docs conflict changes

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "./",
   plugins: [react()],
-  build: {
-    outDir: "docs",
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/[name].js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name][extname]",
-      },
-    },
-  },
+  build: { outDir: "docs" },
 });
